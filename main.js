820893bc756d166d15c5043cdb493e13c06501e7
jokay03J-V2 <jmagro@edenschool.fr>
2023-03-13 16:10:25 +0100

    update main script file

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
 import "./style.css";
+const foodsContainer = document.getElementById("foods");
+const plats = [
+  {
+    imgSrc: "./assets/img-01.jpg",
+    title: "un titre super",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus architectominima assumenda perferendisqui consequatur dicta, a cumque cupiditate, rerum voluptates animi illo corrupti modi esse at. Ipsum,ducimus delectus.",
+  },
+  {
+    imgSrc: "./assets/img-02.jpg",
+    title: "un titre super",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus architectominima assumenda perferendisqui consequatur dicta, a cumque cupiditate, rerum voluptates animi illo corrupti modi esse at. Ipsum,ducimus delectus.",
+  },
+  {
+    imgSrc: "./assets/img-03.jpg",
+    title: "un titre super",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus architectominima assumenda perferendisqui consequatur dicta, a cumque cupiditate, rerum voluptates animi illo corrupti modi esse at. Ipsum,ducimus delectus.",
+  },
+  {
+    imgSrc: "./assets/img-04.jpg",
+    title: "un titre super",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus architectominima assumenda perferendisqui consequatur dicta, a cumque cupiditate, rerum voluptates animi illo corrupti modi esse at. Ipsum,ducimus delectus.",
+  },
+  {
+    imgSrc: "./assets/img-05.jpg",
+    title: "un titre super",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus architectominima assumenda perferendisqui consequatur dicta, a cumque cupiditate, rerum voluptates animi illo corrupti modi esse at. Ipsum,ducimus delectus.",
+  },
+  {
+    imgSrc: "./assets/img-06.jpg",
+    title: "un titre super",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus architectominima assumenda perferendisqui consequatur dicta, a cumque cupiditate, rerum voluptates animi illo corrupti modi esse at. Ipsum,ducimus delectus.",
+  },
+];
+
+for (let indexPlats = 0; indexPlats < plats.length; indexPlats++) {
+  const plat = plats[indexPlats];
+  foodsContainer.innerHTML += `<article class="food"><img src=${plat.imgSrc} alt="food 1" class="food--img">
+        <div class="food--infos">
+          <h3 class="food--title">${plat.title}</h3>
+          <p class="food--description">${plat.text}</p>
+        </div>
+      </article>`;
+}
+
